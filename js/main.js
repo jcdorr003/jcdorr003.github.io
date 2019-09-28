5d38a7131ad7334a9f1d19aed83b1b99f11d0205
J.C. Dorr <jcdorr3@JCs-MacBook-Pro.home>
2019-09-28 13:21:39 -0400

    Changed header and nav.

--- js/main.js
+++ js/main.js
@@ -1,41 +1,28 @@
-// sticky navbar function
-const nav = document.querySelector("nav");
-const topOfNav = nav.offsetTop;
-
-function stickyNav() {
-  if (window.scrollY >= topOfNav) {
-    document.body.style.paddingTop = nav.offsetHeight + 'px';
-    document.body.classList.add('sticky-nav');
-  } else {
-    document.body.style.paddingTop = 0;
-    document.body.classList.remove('sticky-nav');
-  }
-}
-
-window.addEventListener('scroll', stickyNav);
-
-// smooth scroll function
+// Sticky nav and smooth scroll function
 $(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
-
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 700, function(){
-
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
+  var docEl = $(document),
+      headerEl = $('header'),
+      headerWrapEl = $('.header-container'),
+      navEl = $('nav'),
+      linkScroll = $('.scroll');
+  
+  docEl.on('scroll', function(){
+    if ( docEl.scrollTop() > 60 ){
+      headerEl.addClass('fixed-to-top');
+      headerWrapEl.addClass('fixed-to-top');
+      navEl.addClass('fixed-to-top');
+    }
+    else {
+      headerEl.removeClass('fixed-to-top');
+      headerWrapEl.removeClass('fixed-to-top');
+      navEl.removeClass('fixed-to-top');
+    }
   });
+  
+  linkScroll.click(function(e){
+      e.preventDefault();
+      $('body, html').animate({
+         scrollTop: $(this.hash).offset().top
+      }, 500);
+   });
 });
